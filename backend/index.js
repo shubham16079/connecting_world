fed589f47474f6651eeecc086dbf2632496339cb
Shubham Singh <Shubham@Vcarenoida.com>
2023-11-29 23:04:06 +0530

    socket changes

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const cors = require('cors');
+const { createServer } = require('node:http');
+const { join } = require('node:path');
+const { Server } = require('socket.io');
 const bodyParser = require('body-parser');
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.emit('hello', 'hello world from server side');
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+  
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
 const port = 5000; 
 const router = express.Router();
 const usersRouter = require('./api/users');
@@ -9,7 +25,10 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false,useNewUrlParser:false }));
 const mongoose = require('./db'); 
-app.listen(port, () => {
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'index.html'));
+});
+server.listen(port, () => { 
   console.log(`Server is running on http://localhost:${port}`);
 });
 app.use('/api/users', usersRouter);
